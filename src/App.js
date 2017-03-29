927d0947e4b7dc26086d7e8828b1d7a4693557dd
francisco15 <franciscohuertas38@gmail.com>
2017-03-29 17:09:19 -0500

    agregada funcionalidad

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.updateText = this.updateText.bind(this);
+    this.state = {
+      repeat: ""
+    }
+  }
+
+  updateText(event){
+    this.setState({
+      repeat: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" value={this.state.repeat} onChange={this.updateText} />
+        <p className="repeater">{this.state.repeat}</p>
       </div>
     );
   }
